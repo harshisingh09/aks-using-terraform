512f30d209dc12ded7b92becaa4a30dde3babc9e
Harshita Singh1 <Harshita_Singh1@epam.com>
2022-08-12 14:57:22 +0530

    modified providers.tf file

--- terraform-manifests/provider.tf
+++ terraform-manifests/provider.tf
@@ -1,28 +1,28 @@
 terraform{
-   required_providers{
-    azurerm={
+   required_providers {
+    azurerm = {
         source = "hashicorp/azurerm"
         version = "~>3.8.0"
     }
-    helm{
+    helm = {
         source = "hashicorp/helm"
         version = "~>2.6.0"
     }
    }
-   backend "azurerm"{
+   backend "azurerm" {
 
    }
 }
 
-provider "azurerm"{
+provider "azurerm" {
     features{
 
     }
 }
 
 provider "helm" {
-    kubernetes{
-        host = 
+    kubernetes {
+        host = module.aks.fqdn
         client_certificate = base64decode(module.aks.client_certificate)
         client_key = base64decode(module.aks.client_key)
         cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
